16efaf62da5b34af1af1ed80fb599fc39db01496
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2022-10-09 08:17:47 +0100

    new design

--- src/components/home/PatronageSection.vue
+++ src/components/home/PatronageSection.vue
@@ -2,6 +2,16 @@
   <div class="patronage">
     <div class="container">
       <div class="row">
+        <div class="col-lg-12">
+          <div class="sub__header text-center">
+            <h2 data-aos="fade-up" class="fw-bolder mb-2">
+              Why <span>Choose</span> Us
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
         <div class="col-lg-6 my-2">
           <div data-aos="fade-up" class="patronage_img">
             <img
@@ -11,17 +21,61 @@
             />
           </div>
         </div>
-        <div class="col-lg-6 my-2">
-          <div class="patronage__details">
-            <h2 data-aos="fade-up" class="fw-bolder text-primary mb-2">
-              Why Choose Us
-            </h2>
 
-            <div
-              data-aos="fade-up"
-              data-aos-delay="50"
-              class="d-flex align-items-baseline gap-1 mb-1"
-            >
+        <div class="col-lg-6 my-2">
+          <div class="row">
+            <div class="col-6">
+              <Icon
+                class="about__pointer"
+                icon="codicon:debug-breakpoint-data"
+                height="30"
+              />
+              <p class="m-0 p-0">
+                At Petigrity Konsult, we have partnered with various health
+                insurers to provide solutions that are specifically tailored to
+                every client's specific need.
+              </p>
+            </div>
+            <div class="col-6">
+              <Icon
+                class="about__pointer"
+                icon="codicon:debug-breakpoint-data"
+                height="30"
+              />
+              <p class="m-0 p-0">
+                At Petigrity Konsult, we have partnered with various health
+                insurers to provide solutions that are specifically tailored to
+                every client's specific need.
+              </p>
+            </div>
+            <div class="col-6">
+              <Icon
+                class="about__pointer"
+                icon="codicon:debug-breakpoint-data"
+                height="30"
+              />
+              <p class="m-0 p-0">
+                At Petigrity Konsult, we have partnered with various health
+                insurers to provide solutions that are specifically tailored to
+                every client's specific need.
+              </p>
+            </div>
+            <div class="col-6">
+              <Icon
+                class="about__pointer"
+                icon="codicon:debug-breakpoint-data"
+                height="30"
+              />
+              <p class="m-0 p-0">
+                At Petigrity Konsult, we have partnered with various health
+                insurers to provide solutions that are specifically tailored to
+                every client's specific need.
+              </p>
+            </div>
+          </div>
+
+          <!-- <div class="patronage__details d-flex flex-wrap gap-1">
+            <div data-aos="fade-up" data-aos-delay="50" class="gap-1 mb-1">
               <div>
                 <Icon
                   class="about__pointer"
@@ -37,11 +91,7 @@
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-up"
-              data-aos-delay="100"
-              class="d-flex align-items-baseline gap-1 mb-1"
-            >
+            <div data-aos="fade-up" data-aos-delay="100" class="gap-1 mb-1">
               <div>
                 <Icon
                   class="about__pointer"
@@ -57,11 +107,7 @@
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-up"
-              data-aos-delay="150"
-              class="d-flex align-items-baseline gap-1 mb-1"
-            >
+            <div data-aos="fade-up" data-aos-delay="150" class="gap-1 mb-1">
               <div>
                 <Icon
                   class="about__pointer"
@@ -78,11 +124,7 @@
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-up"
-              data-aos-delay="150"
-              class="d-flex align-items-baseline gap-1 mb-1"
-            >
+            <div data-aos="fade-up" data-aos-delay="150" class="gap-1 mb-1">
               <div>
                 <Icon
                   class="about__pointer"
@@ -98,7 +140,7 @@
                 </p>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -112,15 +154,14 @@ import { Icon } from "@iconify/vue";
 <style lang="css">
 .patronage {
   position: relative;
-  padding: 3rem 0;
+  padding-top: 5rem;
 }
 
-.patronage__details h2 {
+.sub__header h2 {
   position: relative;
-  font-size: 3.5rem;
-  -webkit-text-size-adjust: auto;
-  -webkit-text-fill-color: transparent;
-  -webkit-text-stroke-width: 2px;
-  -webkit-text-stroke-color: var(--blue-1);
+}
+.sub__header h2 span {
+  position: relative;
+  color: var(--yellow-1);
 }
 </style>
