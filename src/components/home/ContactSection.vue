6feaffb5294a80194ac352650b30444d30d5fdb7
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2022-10-12 06:58:02 +0100

    changes to contact section

--- src/components/home/ContactSection.vue
+++ src/components/home/ContactSection.vue
@@ -1,128 +1,19 @@
 <template>
   <div id="contact">
     <div class="container">
-      <SubHeader data-aos="fade-up">
+      <!-- <SubHeader data-aos="fade-up">
         <h2 class="fw-bolder mb-2"><span>Contact</span> Us</h2>
-      </SubHeader>
+      </SubHeader> -->
 
-      <div class="row">
-        <div class="col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="50">
-          <div class="contact__details px-2 pt-2">
-            <h3 class="h2 text-primary fw-bolder">Contact Information</h3>
-
-            <div class="row mt-2">
-              <div class="col-12 my-1">
-                <div
-                  class="
-                    shadow
-                    bg-light
-                    rounded
-                    details
-                    d-flex
-                    flex-column
-                    justify-content-center
-                    align-items-center
-                    p-1
-                  "
-                >
-                  <img loading='lazy'
-                    src="../../assets/images/icons/telephone.png"
-                    alt="phone icon"
-                    width="24"
-                    height="24"
-                  />
-                  <p class="fw-bold mb-0 mt-1">+234 812 461 4434</p>
-                </div>
-              </div>
-              <div class="col-12 my-1">
-                <div
-                  class="
-                    shadow
-                    bg-light
-                    rounded
-                    details
-                    d-flex
-                    flex-column
-                    justify-content-center
-                    align-items-center
-                    p-1
-                  "
-                >
-                  <img loading='lazy'
-                    src="../../assets/images/icons/message.png"
-                    alt="phone icon"
-                    width="24"
-                    height="24"
-                  />
-                  <p class="fw-bold mb-0 mt-1">sales@perigrity.com</p>
-                </div>
-              </div>
-            </div>
-
-            <div class="text-center">
-              <img loading='lazy'
-                src="../../assets/images/img/contact.svg"
-                alt=""
-                class="w-50 contact__img img-fluid"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-2" data-aos="fade-up" data-aos-delay="100">
-          <div class="contact__form p-2 shadow rounded bg-white">
-            <h4 class="fw-bold">
-              Fill the form and we will get back to you shortly
-            </h4>
-
-            <div class="mt-3">
-              <div class="row my-2">
-                <div class="col-6">
-                  <label for="" class="label-control">Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Enter your name"
-                  />
-                </div>
-                <div class="col-6">
-                  <label for="" class="label-control">Email</label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    placeholder="Enter your email"
-                  />
-                </div>
-              </div>
-              <div class="row my-2">
-                <div class="col-6">
-                  <label for="" class="label-control">Phone</label>
-                  <input
-                    type="tel"
-                    class="form-control"
-                    placeholder="Enter your phone number"
-                  />
-                </div>
-                <div class="col-6">
-                  <label for="" class="label-control">Subject</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Subject"
-                  />
-                </div>
-              </div>
-              <div class="form-row my-2">
-                <label for="" class="label-control">Message</label>
-                <textarea rows="5" class="form-control"></textarea>
-              </div>
-              <div class="form-row my-2">
-                <button
-                  class="btn btn-outline-primary btn-lg shadow-lg rounded-pill"
-                >
-                  Send
-                </button>
-              </div>
-            </div>
+      <div class="row d-flex align-items-center justify-content-center">
+        <div class="col-lg-12">
+          <div class="contact__box text-center p-3 rounded shadow-lg">
+            <h3 class="text-white fw-bolder">Get it touch</h3>
+            <router-link
+              class="btn btn-lg my-2 btn-outline-light rounded"
+              :to="{ name: 'contact' }"
+              >Contact Us</router-link
+            >
           </div>
         </div>
       </div>
@@ -136,20 +27,9 @@ import SubHeader from "@/components/home/SubHeader.vue";
 </script>
 
 <style lang="css" scoped>
-#contact {
-  padding-top: 5rem;
-  position: relative;
-}
-.contact__details {
-  background: url("../../assets/images/bg/contact-bg.svg") no-repeat;
-  background-position: center;
+.contact__box {
+  background: url("../../assets/images/bg/contact-bg.jpg") no-repeat;
+  /* background-position: center; */
   background-size: cover;
-  border-radius: 5px;
-}
-.details {
-  border: 1.5px solid var(--blue-0);
-}
-.contact__img {
-  margin-bottom: -60px;
 }
 </style>
