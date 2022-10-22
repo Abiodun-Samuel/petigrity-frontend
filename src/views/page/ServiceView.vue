c2a4c818cb2483e35ade7d51da90b2f425897939
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2022-10-22 22:33:46 +0100

    services page

--- src/views/page/ServiceView.vue
+++ src/views/page/ServiceView.vue
@@ -9,19 +9,18 @@
         <div
           data-aos-delay="50"
           data-aos="fade-up"
-          class="col-lg-6 col-md-6 my-2 d-flex align-items-stretch"
+          class="col-lg-4 col-md-6 my-2 d-flex align-items-stretch"
         >
           <div class="service__details text-center p-2 rounded">
             <img
               loading="lazy"
-              src="../../assets/images/icons/svg/insurance.svg"
+              src="../../assets/images/icons/svg/advice.svg"
               width="120"
               alt="service section icon"
               class=""
             />
             <h4 class="fw-bolder text-primary">
               Professional Impartial Advice
-              <!-- Health Insurance -->
             </h4>
             <p>
               We make impartial recommendations based on you, or your business’s
@@ -40,21 +39,48 @@
         <div
           data-aos-delay="100"
           data-aos="fade-up"
-          class="col-lg-6 col-md-6 my-2 d-flex align-items-stretch"
+          class="col-lg-4 col-md-6 my-2 d-flex align-items-stretch"
         >
           <div class="service__details text-center p-2 rounded">
             <img
               loading="lazy"
-              src="../../assets/images/icons/svg/training.svg"
+              src="../../assets/images/icons/svg/support.svg"
               width="120"
               alt="service section icon"
               class=""
             />
             <h4 class="fw-bolder text-primary">Additional Client Support</h4>
             <p>
-              We also offer a wide range of additional, complementary services
-              such as claims processing assistance, current plan review,
-              renewal, and many more, all at no additional cost.
+              Improved customer service and additional customer support, smooth
+              policy buying and transitioning process from one HMO to another.
+            </p>
+            <div class="">
+              <router-link
+                class="btn btn-outline-primary rounded-pill"
+                :to="{ name: 'service.training' }"
+                >Read More</router-link
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          data-aos-delay="150"
+          data-aos="fade-up"
+          class="col-lg-4 col-md-6 my-2 d-flex align-items-stretch"
+        >
+          <div class="service__details text-center p-2 rounded">
+            <img
+              loading="lazy"
+              src="../../assets/images/icons/svg/general.svg"
+              width="120"
+              alt="service section icon"
+              class=""
+            />
+            <h4 class="fw-bolder text-primary">Complementary Services</h4>
+            <p>
+              We also offer a wide range of complementary services such as
+              claims processing assistance, current plan review, renewal, and
+              many more, all at no additional cost.
             </p>
             <div class="">
               <router-link
@@ -66,98 +92,67 @@
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="row d-flex justify-content-center align-items-stretch">
-        <div data-aos-delay="150" data-aos="fade-up" class="col-lg-6 my-2">
-          <div class="text-center p-2 rounded">
-            <h4 class="fw-bolder text-warning my-2">Others Services.</h4>
-
-            <div class="accordion" id="accordionExample">
-              <div class="accordion-item my-1">
-                <h2 class="accordion-header" id="headingOne">
-                  <button
-                    class="accordion-button"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseOne"
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                  >
-                    <img
-                      loading="lazy"
-                      src="../../assets/images/icons/svg/training.svg"
-                      width="100"
-                      alt="service section icon"
-                    />
-                    <h4 class="ms-2 fw-bolder text-primary">Training</h4>
-                  </button>
-                </h2>
-                <div
-                  id="collapseOne"
-                  class="accordion-collapse collapse show"
-                  aria-labelledby="headingOne"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div class="accordion-body pb-3">
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Quibusdam illum dolorem ea placeat cupiditate fuga
-                      molestias ipsum officiis quaerat veniam numquam odit iste,
-                      assumenda consequuntur.
-                    </p>
-
-                    <div class="">
-                      <router-link
-                        class="btn btn-outline-primary rounded-pill"
-                        :to="{ name: 'service.recruitment' }"
-                        >Read More</router-link
-                      >
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="accordion-item my-1">
-                <h2 class="accordion-header" id="headingTwo">
-                  <button
-                    class="accordion-button collapsed"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseTwo"
-                    aria-expanded="false"
-                    aria-controls="collapseTwo"
-                  >
-                    <img
-                      loading="lazy"
-                      src="../../assets/images/icons/svg/recruitment.svg"
-                      width="100"
-                      alt="service section icon"
-                    />
-                    <h4 class="ms-2 fw-bolder text-primary">Recruitment</h4>
-                  </button>
-                </h2>
-                <div
-                  id="collapseTwo"
-                  class="accordion-collapse collapse"
-                  aria-labelledby="headingTwo"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div class="accordion-body pb-3">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Voluptatum commodi provident voluptate ipsa vero
-                      exercitationem maxime ab dolores assumenda unde!
-                    </p>
-
-                    <div class="">
-                      <router-link
-                        class="btn btn-outline-primary rounded-pill"
-                        :to="{ name: 'service.recruitment' }"
-                        >Read More</router-link
-                      >
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div class="other__services mt-3">
+      <SubHeader>
+        <h2 class="fw-bolder my-4">Others <span>Services</span></h2>
+      </SubHeader>
+      <div class="container">
+        <div class="row">
+          <div data-aos-delay="200" data-aos="fade-up" class="col-lg-4 my-2">
+            <div class="other__services__details">
+              <img
+                loading="lazy"
+                src="../../assets/images/icons/svg/training.svg"
+                width="300"
+                alt="service section icon"
+                class="img-fluid"
+              />
+            </div>
+          </div>
+          <div
+            data-aos-delay="250"
+            data-aos="fade-up"
+            class="col-lg-6 my-2 d-flex align-items-center"
+          >
+            <div class="other__services__details">
+              <h4 class="fw-bolder text-primary">Training</h4>
+              <p>
+                At Petigrity Konsult, we conduct high-impact training programs
+                which encompasses a broad range of staff requirements and needs
+                – including professional development and industry-relevant
+                subjects – while leveraging expertise in business areas
+                including negotiation, team building, and leadership.
+              </p>
+            </div>
+          </div>
+          <div data-aos-delay="250" data-aos="fade-up" class="col-lg-4 my-2">
+            <div class="other__services__details">
+              <img
+                loading="lazy"
+                src="../../assets/images/icons/svg/recruitment.svg"
+                width="300"
+                alt="service section icon"
+                class="img-fluid"
+              />
+            </div>
+          </div>
+          <div
+            data-aos-delay="300"
+            data-aos="fade-up"
+            class="col-lg-6 my-2 d-flex align-items-center"
+          >
+            <div class="other__services__details">
+              <h4 class="fw-bolder text-primary">Recruitment</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                fugiat ipsam, ea impedit dolorum deleniti eveniet quasi
+                molestiae facilis sequi eum sed, quis veritatis eos
+                reprehenderit sapiente, nihil natus ab nobis fugit perspiciatis
+                deserunt eaque. Itaque exercitationem voluptas consectetur
+                eveniet.
+              </p>
             </div>
           </div>
         </div>
@@ -168,7 +163,11 @@
 
 <script setup>
 import PageHeader from "@/components/pages/PageHeader.vue";
+import SubHeader from "@/components/home/SubHeader.vue";
 </script>
 
 <style lang="css">
+.other__services {
+  background: var(--blue-0);
+}
 </style>
