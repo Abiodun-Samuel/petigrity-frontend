e5f0913b53acaca8abcf36c600069a0f5a0e1030
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2022-11-01 08:44:45 +0100

    bootstrap-min.css

--- src/layouts/home/HeaderComponent.vue
+++ src/layouts/home/HeaderComponent.vue
@@ -38,10 +38,97 @@
           </li>
 
           <li class="nav-item mx-lg-3 my-lg-1">
-            <router-link class="nav-link" :to="{ name: 'services' }"
-              >Services</router-link
+            <a
+              class="nav-link"
+              v-if="show"
+              key="on"
+              @click="show = false"
+              href="JavaScript:Void(0)"
             >
+              <span>Services</span> <Icon icon="bx:down-arrow" />
+            </a>
+            <a
+              class="nav-link"
+              href="JavaScript:Void(0)"
+              v-else
+              key="off"
+              @click="show = true"
+            >
+              <span>Services</span> <Icon icon="bx:right-arrow" />
+            </a>
+            <!-- Dropdown Menu -->
+            <transition name="dropdown">
+              <div
+                class="dropdown__menu shadow-lg"
+                v-bind:class="{ active: show }"
+                v-if="show"
+              >
+                <ul class="dropdown__menu-nav">
+                  <li class="dropdown__menu-item">
+                    <p class="text-primary fw-bolder my-1">Health Insurance</p>
+                    <router-link
+                      @click="show = false"
+                      :to="{}"
+                      class="dropdown__menu-link"
+                    >
+                      <div class="dropdown__menu-text">
+                        <Icon class="me-1" icon="bx:right-arrow-circle" />
+                        Professional Impartial Advice
+                      </div>
+                    </router-link>
+                    <router-link
+                      @click="show = false"
+                      :to="{}"
+                      class="dropdown__menu-link"
+                    >
+                      <div class="dropdown__menu-text">
+                        <Icon class="me-1" icon="bx:right-arrow-circle" />
+                        Additional Client Support
+                      </div>
+                    </router-link>
+                    <router-link
+                      @click="show = false"
+                      :to="{}"
+                      class="dropdown__menu-link"
+                    >
+                      <div class="dropdown__menu-text">
+                        <Icon class="me-1" icon="bx:right-arrow-circle" />
+                        Complementary Services
+                      </div>
+                    </router-link>
+                  </li>
+                  <li class="dropdown__menu-item">
+                    <p class="text-primary fw-bolder my-1">Other Services</p>
+                    <router-link
+                      @click="show = false"
+                      :to="{ name: 'service.training' }"
+                      class="dropdown__menu-link"
+                    >
+                      <div class="dropdown__menu-text">
+                        <Icon
+                          class="me-1"
+                          icon="bx:right-arrow-circle"
+                        />Training
+                      </div>
+                    </router-link>
+                    <router-link
+                      @click="show = false"
+                      :to="{ name: 'service.recruitment' }"
+                      class="dropdown__menu-link"
+                    >
+                      <div class="dropdown__menu-text">
+                        <Icon
+                          class="me-1"
+                          icon="bx:right-arrow-circle"
+                        />Recruitment
+                      </div>
+                    </router-link>
+                  </li>
+                </ul>
+              </div>
+            </transition>
           </li>
+
           <li class="nav-item mx-lg-3 my-lg-1">
             <router-link class="nav-link" :to="{ name: 'contact' }"
               >Contact Us</router-link
@@ -63,24 +150,6 @@
       </div>
     </div>
   </nav>
-
-  <!-- <li class="nav-item dropdown">
-    <a
-      class="nav-link dropdown-toggle"
-      href="#"
-      role="button"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Dropdown
-    </a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><hr class="dropdown-divider" /></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-  </li> -->
 
   <BaseModal
     data-aos="zoom-in"
@@ -105,14 +174,71 @@ import BaseModal from "@/components/Base/BaseModal.vue";
 import GetStarted from "@/components/partials/GetStarted.vue";
 
 const openGetStartedModal = ref(false);
+const show = ref(false);
 </script>
 
 <style lang="css">
-/* ul.dropdown-menu.show {
-  height: 200px !important;
-  background: red;
+.dropdown__menu {
+  top: 100%;
+  position: absolute;
+  z-index: 10;
+  min-width: 300px;
+  margin-top: 1rem;
+  overflow-y: auto;
+  padding: 1rem;
+  border-radius: 12px;
+  background-color: white;
+  border: 1px solid #ffffff;
+  background-clip: padding-box;
 }
-a.dropdown-item {
-  height: 23px;
-} */
+.dropdown__menu-link {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  text-decoration: none;
+  color: rgb(139, 139, 139);
+  padding: 0.5rem;
+  margin-top: 0.2rem;
+  margin-bottom: 0.2rem;
+  border-radius: 0 50px 50px 0;
+}
+.dropdown__menu-link:hover {
+  color: var(--blue-1);
+  background-color: var(--blue-0);
+}
+
+.dropdown__menu-text {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.6s;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-active {
+  opacity: 0;
+}
+
+.slide-fade-enter {
+  transform: translateX(31px);
+}
+
+.slide-fade-leave-active {
+  transform: translateX(-31px);
+}
+
+.dropdown-enter-active,
+.dropdown-leave-active {
+  transition: all 1s;
+}
+
+.dropdown-enter,
+.dropdown-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
